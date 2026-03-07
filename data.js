4a0740ea38615961316e5bb1416563db0ce63204
Londrovski <jrmat49@gmail.com>
2026-03-07 23:05:30 +0000

    Fix Azrael image: .jpeg → .png

--- data.js
+++ data.js
@@ -94,7 +94,7 @@ const PLAYERS = [
   {
     id: 'azrael',
     name: 'Azrael',
-    image: 'Azrael.jpeg',
+    image: 'Azrael.png',
     player: 'Butch',
     description: 'A bloodhunter operating at the edge of what his order sanctions. Azrael has powers he doesn\'t fully understand yet, and a past that has not finished catching up with him.'
   },
