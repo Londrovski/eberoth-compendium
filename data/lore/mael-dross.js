3a78588cd6049607e96a6933a0cb00e1bb172726
Londrovski <jrmat49@gmail.com>
2026-05-26 11:01:04 +0100

    Add image: Mael Dross

--- data/lore/mael-dross.js
+++ data/lore/mael-dross.js
@@ -2,5 +2,6 @@
   id: 'mael-dross',
   name: 'High Inquisitor Mael Dross',
   subtitle: 'High Inquisitor of the Crown.',
+  image: 'Mael Dross.png',
   body: 'High Inquisitor in service of the Crown. Conducted the inquest following the recovery of the Halvorn ledger.\n\nQuiet, careful, deliberate. The kind of man who already has the answer before he asks the question.\n\nHis interest in the <strong>Stillmarks</strong> has not been explained.'
 });
