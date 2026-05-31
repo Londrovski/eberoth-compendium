0e1926a7928185381234e8ba99fd46c1b0c05692
Londrovski <jrmat49@gmail.com>
2026-05-31 10:08:38 +0100

    MainLayout: mount DmHighlightBanner + bootstrap dm-highlight store

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,14 +5,26 @@
       <router-view />
     </q-page-container>
     <DetailPanel />
+    <DmHighlightBanner />
   </q-layout>
 </template>
 
 <script setup>
 // Main layout. Topbar + a router-view + the global DetailPanel
 // mounted once so any component can call useEntityDetail.open(id).
+// Also mounts DmHighlightBanner for players (it self-hides for DM).
+import { onMounted } from 'vue';
 import TopBar from 'components/topbar/TopBar.vue';
 import DetailPanel from 'components/detail/DetailPanel.vue';
+import DmHighlightBanner from 'components/banner/DmHighlightBanner.vue';
+import { useDmHighlightStore } from 'src/stores/dm-highlight';
+
+const dmHighlight = useDmHighlightStore();
+
+onMounted(async () => {
+  await dmHighlight.load();
+  dmHighlight.subscribeRealtime();
+});
 </script>
 
 <style scoped>
